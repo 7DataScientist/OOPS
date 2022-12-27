f01f7bdcb9227f68635508462ff072cb63ef59bf
7DataScientist <113970046+7DataScientist@users.noreply.github.com>
2022-12-27 13:07:31 +0530

    Add files via upload

---
+++ 12_27_2022_OOPS/test.py
@@ -0,0 +1,12 @@
+from gen import lst
+from gen2 import person1
+from gen3 import person2
+
+# p1 = lst([1,2,3,4,5,6,7,8,9])
+# print(p1.odd1())
+
+p1 = person1("James","Cook",1991)
+p2 = person2("Albert","Enistein",1985)
+
+print(p1._name1)
+print(p2._name1)
